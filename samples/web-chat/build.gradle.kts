2ffc9b8cbb3cff80799a67e4e46c3523a54fb3a0
fengysen@gmail.com <fengysen@gmail.com>
2026-08-28 01:15:47 +0800

    Resolve Web dependency advisories
    [skip ci]

--- samples/web-chat/build.gradle.kts
+++ samples/web-chat/build.gradle.kts
@@ -1,8 +1,22 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
+import org.jetbrains.kotlin.gradle.targets.wasm.yarn.WasmYarnRootExtension
 
 plugins {
     kotlin("multiplatform") version "2.4.0"
 }
+
+val secureWebpackVersion = "5.104.1"
+val secureWsVersion = "8.21.0"
+
+YarnRootExtension[rootProject].apply {
+    resolution("ws", secureWsVersion)
+}
+WasmYarnRootExtension[rootProject].apply {
+    resolution("ws", secureWsVersion)
+}
+rootProject.extensions.getByType<NodeJsRootExtension>().versions.webpack.version = secureWebpackVersion
 
 val magratheaVersion = providers.gradleProperty("magrathea.version").orElse("0.1.0-alpha.6")
 providers.gradleProperty("magrathea.consumer.buildDir").orNull?.let { consumerBuildDirectory ->
